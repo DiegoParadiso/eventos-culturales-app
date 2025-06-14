f33b5a4280738749bd3187123757670f66363b6e
DiegoParadiso <diego.gonzaaleez.a@gmail.com>
2025-06-14 10:07:28 -0300

    Integracion Maven

--- src/main/java/app/AppFx.java
+++ src/main/java/app/AppFx.java
@@ -1,21 +1,16 @@
 package app;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class AppFX extends Application {
     @Override
-    public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/MainView.fxml"));
-        Scene scene = new Scene(loader.load());
-        stage.setTitle("Gestión de Eventos Culturales");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("App JavaFX funcionando!");
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
